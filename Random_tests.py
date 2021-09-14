0547777302eb0e5e73c7c1d03ba6778d3eaebafc
adrikosm <adrikosm@gmail.com>
2021-09-14 21:10:05 +0200

    Random Tests

--- Random_tests.py
+++ Random_tests.py
@@ -3,4 +3,13 @@ total = 0
 
 for i in list_of_nums:
     if len(str(i)) % 2 == 0:
-        print(i)
+        print(f"List of nums with even digits {i}")
+
+nums = [-4, -1, 0, 3, 10]
+sorted_list = []
+
+for i in nums:
+    sorted_list.append(abs(i) ** 2)
+
+for i in sorted_list:
+    print(f"Sorted list squared:{i}")
