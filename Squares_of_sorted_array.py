b5dbc00b746147823cc9166732e54f0eb249c10b
adrikosm <adrikosm@gmail.com>
2021-09-14 21:11:14 +0200

    Squared of sorted array

---
+++ Squares_of_sorted_array.py
@@ -0,0 +1,37 @@
+"""
+Given an integer array nums sorted in non-decreasing order, return an array of the
+squares of each number sorted in non-decreasing order.
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+
+"""
+from timeit import timeit
+
+
+def sorted_squares_simple(nums):
+    sorted_list = []
+    for i in nums:
+        sorted_list.append(abs(i) ** 2)
+    return sorted(sorted_list)
+
+
+def sortedSquares(nums):
+    res = sorted([n * n for n in nums])
+    return res
+
+
+def main():
+    nums = [-4, -1, 0, 3, 10]
+    sorted_first = timeit(lambda: sorted_squares_simple(nums))
+    sorted_second = timeit(lambda: sortedSquares(nums))
+    print(f"{sorted_first = :0.2f}")
+    print(f"{sorted_second = :0.2f}")
+
+
+if __name__ == "__main__":
+    main()
